766adecbe2cfae1bc1975f1579833881972740f2
kumaresh1597 <kumaresh.1597@gmail.com>
2023-05-11 23:14:21 +0530

    changed class

--- src/main/java/com/driver/A.java
+++
@@ -1,8 +0,0 @@
-package com.driver;
-//Task 1
-public class A {
-
-    public String meth(){
-        return "Invoking method from class A";
-    }
-}
--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,30 @@
 package com.driver;
 
 public class Main {
-  
+    public static class A {
+        public String meth(){
+            return "Invoking method from class A";
+        }
+    }
+
+    public static class B extends A {
+        public String meth(){
+            return "Method is overridden in Extendend class B";
+        }
+    }
+
+    public static void main(String[] args) {
+        B obj = new B();
+
+        String s = obj.meth();
+
+//        Before overring
+//        System.out.println(s)
+//        Invoking method from class A
+
+
+//        After overriding
+        System.out.println(s);
+//        Method is overridden in Extendend class B
+    }
 }
